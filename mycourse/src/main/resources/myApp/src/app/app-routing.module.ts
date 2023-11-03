e4f6dea46b053d1a6e54b0d977404844b8bff317
LucasCarneiro99 <lucasbcarneiro903@gmail.com>
2023-11-02 22:52:22 -0300

    primeiras mudancas no site, necessario ajuste na altura do site

--- mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
+++ mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", redirectTo:"login", pathMatch: 'full'
+  },
+  {
+    path: "login", component:LoginComponent
+  },
+  {
+    path: "home", component:PaginaInicialComponent
+  }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
