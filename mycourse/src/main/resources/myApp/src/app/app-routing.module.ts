2279ef665ccc842cfd63ed13c4cb10798890d175
Guilherme Chaves <guilherme.chaves66@gmail.com>
2023-11-06 16:31:38 -0300

    Nova Pagina Inicial

--- mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
+++ mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
@@ -1,24 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
 
 const routes: Routes = [
-  {
-    path: "", redirectTo:"login", pathMatch: 'full'
-  },
-  {
-    path: "login", component:LoginComponent
-  },
-  {
-    path: "home", component:PaginaInicialComponent
-  }
-
+  { path: "", redirectTo: "login", pathMatch: 'full' },
+  { path: "login", component: LoginComponent },
+  { path: "home", component: PaginaInicialComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
