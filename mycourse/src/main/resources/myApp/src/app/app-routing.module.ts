1a76b9dec661f520e9421ea118105ec4b82a0ed5
Guilherme Chaves <guilherme.chaves66@gmail.com>
2023-11-16 13:40:34 -0300

    Página de Vídeos

--- mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
+++ mycourse/src/main/resources/myApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { PaginaDeReproducaoDeVideosComponent } from './pages/pagina-de-reproducao-de-videos/pagina-de-reproducao-de-videos.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: 'full' },
   { path: "login", component: LoginComponent },
   { path: "home", component: PaginaInicialComponent },
+  { path: "video", component: PaginaDeReproducaoDeVideosComponent },
 ];
 
 @NgModule({
---
+++ mycourse/src/main/resources/myApp/src/app/pages/pagina-de-reproducao-de-videos/pagina-de-reproducao-de-videos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-de-reproducao-de-videos',
+  templateUrl: './pagina-de-reproducao-de-videos.component.html',
+  styleUrls: ['./pagina-de-reproducao-de-videos.component.scss']
+})
+export class PaginaDeReproducaoDeVideosComponent {
+
+}
